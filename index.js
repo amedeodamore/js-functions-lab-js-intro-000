780c8dacdae5431b6a748007c3b595ce00ed9a73
Amedeo d'Amore <amedeodamore@gmail.com>
2021-01-21 22:35:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ return `Happy holidays, ${name}!`
 function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
